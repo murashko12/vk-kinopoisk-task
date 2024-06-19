1d64bd3cd04848c79c9b9941f5bdfa6676c2df1c
murashko12 <pert@ocumare.ru>
2024-06-19 22:38:24 +0300

    add filtration

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -5,19 +5,25 @@ import { IMovieData } from '../types/IMovieData';
 import { IoMdArrowRoundBack, IoMdArrowRoundForward } from 'react-icons/io';
 
 const Movies: React.FC = () => {
+
     const [movies, setMovies] = useState<IMovieData[]>([])
+
+    const [filteredMovies, setFilteredMovies] = useState<IMovieData[]>([]);
+    const [ratingFilter, setRatingFilter] = useState<number | null>(null);
+    const [yearFilter, setYearFilter] = useState<number | null>(null);
+    // const [genresFilter, setGenresFilter] = useState<IGenre | null>(null)
 
     useEffect(() => {
         const fetchMovieData = async () => {
             try {
                     const movieIds = []
-                    for(let i = 299; i <= 307; i++) {
+                    for(let i = 299; i <= 305; i++) {
                         movieIds.push(i)
                     }
                     const requests = movieIds.map((id) =>
                         axios.get(`https://api.kinopoisk.dev/v1.4/movie/${id}`, {
                           headers: {
-                            'X-API-KEY': import.meta.env.VITE_API_KEY_3,
+                            'X-API-KEY': import.meta.env.VITE_API_KEY_2,
                             'accept': 'application/json',
                           },
                         })
@@ -38,10 +44,9 @@ const Movies: React.FC = () => {
     
     // Pagination
     const [currentPage, setCurrentPage] = useState(1)
-    const recordsPerPage = 50
+    const recordsPerPage = 6
     const lastIndex = currentPage * recordsPerPage
     const firstIndex = lastIndex - recordsPerPage
-    const records = movies.slice(firstIndex, lastIndex)
     const nPage = Math.ceil(movies.length / recordsPerPage)
     const numbersPages = [...Array(nPage + 1).keys()].slice(1)
 
@@ -61,10 +66,67 @@ const Movies: React.FC = () => {
         }
     }
 
+    // filter
+
+    useEffect(() => {
+        const filtered = movies.filter((movie) => {
+            if (ratingFilter !== null && movie.rating.kp < ratingFilter) {
+                return false;
+            }
+            if (yearFilter !== null && movie.year <= yearFilter) {
+                return false;
+            }
+            // if (genresFilter !== null && movie.genres.includes(genresFilter))
+            // return true;
+        });
+            setFilteredMovies(filtered);
+        }, [movies, ratingFilter, yearFilter])
+    
+        const handleRatingFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+            setRatingFilter(event.target.value ? Number(event.target.value) : null);
+        }
+    
+        const handleYearFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+            setYearFilter(event.target.value ? Number(event.target.value) : null);
+        }
+
+        // const handleGenresFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        //     setGenresFilter(event.target.value || null)
+        // }
+    
     return (
         <div className="flex flex-col items-center mt-24">
+            <div className="flex items-center justify-around rounded-lg bg-black w-[60%] mb-8 h-16">
+                <div>
+                    <label htmlFor="ratingFilter" className="text-white">РЕЙТИНГ: </label>
+                    <select id="ratingFilter" value={ratingFilter || ''} onChange={handleRatingFilterChange}>
+                        <option value="">Все</option>
+                        <option value="5">5 и выше</option>
+                        <option value="7">7 и выше</option>
+                        <option value="8">8 и выше</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="yearFilter" className="text-white">ГОД: </label>
+                    <select id="yearFilter" value={yearFilter || ''} onChange={handleYearFilterChange}>
+                        <option value="">Все</option>
+                        <option value="1990">1990</option>
+                        <option value="2000">2000</option>
+                        <option value="2010">2010</option>
+                    </select>
+                </div>
+                {/* <div> */}
+                    {/* <label className="text-white" htmlFor="ratingFilter">ЖАНРЫ: </label>
+                    <select  id="ratingFilter">
+                        <option value="">Все</option>
+                        <option value="5">5 и выше</option>
+                        <option value="7">7 и выше</option>
+                        <option value="8">8 и выше</option>
+                    </select> */}
+                {/* </div> */}
+            </div>
             <ul className="grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3">
-                {records.map((movie) => (
+                {filteredMovies.slice(firstIndex, lastIndex).map((movie) => (
                     <li key={movie.id}>
                         <MovieCard
                             id={movie.id}
@@ -74,7 +136,8 @@ const Movies: React.FC = () => {
                             poster={movie.poster}
                             countries={[]}
                             shortDescription={''}
-                            persons={[]} genres={[]}                        />
+                            persons={[]} genres={[]}                        
+                        />
                     </li>
                 ))}
             </ul>
